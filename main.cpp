37087cae970bc6a8a2da5f3ec03099709145e6b9
M-UsmanShafique <mrcyber464@gmail.com>
2025-12-06 14:16:00 +0300

    Changing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 /*Write a program for a bakery inventory.
 Read N items and quantity left of each.
 If quantity < 8 print “Reorder Needed”.
