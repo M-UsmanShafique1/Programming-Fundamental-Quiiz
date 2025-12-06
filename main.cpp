b892df7ebf2a889c9b03d3ae476cf61ca97b3de5
M-UsmanShafique <mrcyber464@gmail.com>
2025-12-06 16:14:53 +0500

    upload file

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+/*Write a program for a bakery inventory.
+Read N items and quantity left of each.
+If quantity < 8 print “Reorder Needed”.
+Count how many items need reorder.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int items,quantity;
+    int reorderCount=0;
+    
+    cout<<"Enter number of items =";
+    cin>>items;
+    
+    for(int i=1; i<=items; i++)//for input
+    {
+        cout<<"Enter quantity"<<i<<"=";
+        cin>>quantity;
+    
+        if(quantity<8)//condition
+         {
+        cout<<"Reorder needed"<<endl;
+        reorderCount++;
+         }
+    }
+    cout<<"Items that need to reorder ="<<reorderCount;
+
+    return 0;
+}
